ac62e3083490b3e61c62b09ed0742275c4cb9bf3
AnderBraun <besrus91@gmail.com>
2023-11-29 21:25:12 +0500

    initiall getter and satter

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,41 +1,84 @@
-import os
 import pytest
-from src.item import Item, InstantiateCSVError
+import os
+from src.item import Item
 
-# Тест на успешное создание объектов Item из CSV
-def test_instantiate_from_csv_success(tmpdir):
-    # Создаем временный файл CSV с корректными данными
-    csv_data = "name,price,quantity\nitem1,10.0,5\nitem2,20.0,3"
-    csv_file = tmpdir.join("items.csv")
-    csv_file.write(csv_data)
 
-    # Переходим во временную директорию, чтобы файл items.csv был виден
-    os.chdir(tmpdir)
+@pytest.fixture
+def sample_item():
+    return Item("SampleItem", 10.0, 5)
 
-    # Вызываем метод instantiate_from_csv и проверяем, что объекты Item созданы
+
+def test_item_creation(sample_item):
+    assert sample_item.name == "SampleItem"
+    assert sample_item.price == 10.0
+    assert sample_item.quantity == 5
+
+
+def test_item_name_length():
+    with pytest.raises(ValueError, match="Длина наименования товара превышает 10 символов."):
+        item = Item("TooLongItemName", 10.0, 5)
+
+
+def test_calculate_total_price(sample_item):
+    assert sample_item.calculate_total_price() == 50.0
+
+
+def test_apply_discount(sample_item):
+    Item.pay_rate = 0.8
+    sample_item.apply_discount()
+    assert sample_item.price == 8.0
+
+
+def test_instantiate_from_csv(tmp_path):
+    # Создаем временный каталог
+    tmp_dir = tmp_path / "test_dir"
+    tmp_dir.mkdir()
+
+    # Создайте образец CSV-файла
+    csv_content = "name,price,quantity\nItem1,10.0,5\nItem2,15.0,3"
+    csv_path = tmp_dir / "items.csv"
+    csv_path.write_text(csv_content)
+
+    # Установить текущий каталог во временный каталог
+    os.chdir(tmp_dir)
+
+    # Протестируйте метод Instantiate_from_csv
     Item.instantiate_from_csv()
-    items = Item.get_all_items()
-    assert len(items) == 2
 
-# Тест на обработку ошибки отсутствия файла items.csv
-def test_instantiate_from_csv_file_not_found(tmpdir):
-    # Переходим во временную директорию, чтобы файл items.csv не был виден
-    os.chdir(tmpdir)
+    # Проверьте, правильно ли загружены элементы
+    assert len(Item.all) == 2
+    assert Item.all[0].name == "Item1"
+    assert Item.all[1].name == "Item2"
 
-    # Вызываем метод instantiate_from_csv и проверяем, что возникло исключение FileNotFoundError
-    with pytest.raises(FileNotFoundError, match="Отсутствует файл item.csv"):
-        Item.instantiate_from_csv()
 
-# Тест на обработку ошибки поврежденного файла items.csv
-def test_instantiate_from_csv_corrupted_file(tmpdir):
-    # Создаем временный файл CSV с некорректными данными (без колонки quantity)
-    csv_data = "name,price\nitem1,10.0\nitem2,20.0"
-    csv_file = tmpdir.join("items.csv")
-    csv_file.write(csv_data)
+def test_string_to_number():
+    assert Item.string_to_number("123") == 123
+    assert Item.string_to_number("12.34") == 12.34
 
-    # Переходим во временную директорию, чтобы файл items.csv был виден
-    os.chdir(tmpdir)
 
-    # Вызываем метод instantiate_from_csv и проверяем, что возникло исключение InstantiateCSVError
-    with pytest.raises(InstantiateCSVError, match="Файл item.csv поврежден"):
-        Item.instantiate_from_csv()
+def test_add_item():
+    item1 = Item("Item1", 10.0, 5)
+    item2 = Item("Item2", 15.0, 3)
+
+    assert Item.all == [item1, item2]
+
+
+def test_get_all_items():
+    item1 = Item("Item1", 10.0, 5)
+    item2 = Item("Item2", 15.0, 3)
+
+    assert Item.get_all_items() == [item1, item2]
+
+
+def test_add_quantity(sample_item):
+    new_item = Item("New Item", 5.0, 2)
+    result_item = sample_item.add_quantity(new_item)
+
+    assert result_item.name == sample_item.name
+    assert result_item.price == sample_item.price
+    assert result_item.quantity == sample_item.quantity + new_item.quantity
+
+
+def test_add_quantity_type_error(sample_item):
+    with pytest.raises(TypeError, match="Неподдерживаемый тип для добавления"):
+        sample_item.add_quantity("InvalidType")
